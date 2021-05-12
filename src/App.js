f4a507cee048e880b5b0305121cb1a988fe91adf
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-05-12 21:09:20 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Counter from './components/Counter';
-
+import Auth from './components/Auth';
+import Header from './components/Header';
+import UserProfle from './components/UserProfile';
 
 function App() {
   return (
-    <Counter />
+    <>
+      <Counter />
+    </>
   );
 }
 
